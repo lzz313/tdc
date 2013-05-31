564a07e6e553773e819b62b1fa575b428f728519
luzz <lzz313@sina.com>
2013-05-31 17:18:20 +0800

    格式调整

--- src/main/java/org/test/tdc/common/HttpClientUtils.java
+++ src/main/java/org/test/tdc/common/HttpClientUtils.java
@@ -30,18 +30,15 @@ public class HttpClientUtils {
 		HttpClient httpclient = new DefaultHttpClient();
 		String result = "";
 		try {
-//			httpclient.getParams().setParameter(
-//					"http.protocol.content-charset", "utf-8");
+			//httpclient.getParams().setParameter("http.protocol.content-charset", "utf-8");
 			// 连接超时
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
+			httpclient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
 			// 读取超时
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.SO_TIMEOUT, 5000);
+			httpclient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 5000);
 
 			HttpGet hg = new HttpGet(url);
 			//模拟浏览器
-//			hg.addHeader("Content-Type", "text/html;charset=utf-8");  
+			//hg.addHeader("Content-Type", "text/html;charset=utf-8");  
 			hg.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31");  
 			String charset = "UTF-8";  
 			hg.setURI(new java.net.URI(url));  
@@ -49,13 +46,13 @@ public class HttpClientUtils {
             HttpEntity entity = response.getEntity();  
             if (entity != null) {  
             	charset = getContentCharSet(entity);
-                // 使用EntityUtils的toString方法，传递默认编码，在EntityUtils中的默认编码是ISO-8859-1  
+                // 使用EntityUtils的toString方法，默认编码是ISO-8859-1  
             	result = EntityUtils.toString(entity, charset);  
             }  
             
 			// 开始执行，发送请求到服务器
-//			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-//			result = httpclient.execute(hg, responseHandler);
+            //ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            //result = httpclient.execute(hg, responseHandler);
 			
 		} finally {
 			httpclient.getConnectionManager().shutdown();
@@ -103,14 +100,11 @@ public class HttpClientUtils {
 		try {
 			HttpPost hp = new HttpPost(url);
 
-			httpclient.getParams().setParameter(
-					"http.protocol.content-charset", "utf-8");
+			httpclient.getParams().setParameter("http.protocol.content-charset", "utf-8");
 			// 连接超时
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
+			httpclient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
 			// 读取超时
-			httpclient.getParams().setParameter(
-					CoreConnectionPNames.SO_TIMEOUT, 5000);
+			httpclient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 5000);
 
 			List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>();
 			for (Map.Entry<String, String> entry : nameValuePair.entrySet()) {
