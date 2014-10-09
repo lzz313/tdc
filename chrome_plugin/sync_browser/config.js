2f7d297054daa49edcc351df36c4b9fe89cb829e
lzz313 <lzz313@mail.com>
2014-10-09 14:19:37 +0800

    add option page

--- chrome_plugin/sync_browser/config.js
+++ chrome_plugin/sync_browser/config.js
@@ -1,2 +1,26 @@
 var flag='stop';
-var cache_url = "http://localhost:8080";
+var cache_url = localStorage["url"];
+
+
+// Saves options to localStorage.
+function save_options() {
+  var urlText = document.getElementById("cache_url");
+  var url = urlText.value;
+  localStorage["url"] = url;
+
+  alert('保存成功');
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var url = localStorage["url"];
+  if (!url) {
+    return;
+  }
+  var urlText = document.getElementById("cache_url");
+  
+  urlText.value = url;
+  
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.querySelector('#save_btn').addEventListener('click', save_options);
